adadda0c287d02fa4f5e1ca5cb5feadfdf4a0706
Edupalli Likhitha Sai <sailikhitha182005@gmail.com>
2026-06-24 13:11:54 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,8 @@
-const API_BASE = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080'
+//const API_BASE = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080'
+const API_BASE =
+  import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080';
 
+console.log("API_BASE =", API_BASE);
 export async function predictColleges(payload) {
   const res = await fetch(`${API_BASE}/api/predict`, {
     method: 'POST',
